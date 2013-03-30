1422549c2c7a0b02d4d8123d0d5d6932a9c95a93
Gabriel <gabriel@planetmail.com>
2013-03-30 05:03:35 -0400

    Remove evergreen config

--- config/evergreen.rb
+++
@@ -1,9 +0,0 @@
-require 'capybara-webkit'
-
-Evergreen.configure do |config|
-  #config.driver = :selenium
-  config.driver = :webkit
-  config.public_dir = 'app'
-  #config.template_dir = 'templates'
-  config.spec_dir = 'spec/javascripts'
-end
